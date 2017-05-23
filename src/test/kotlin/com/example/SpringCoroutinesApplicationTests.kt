abad334dd989e940d17b0594c8a23fa30d483307
Alexey Nesterov <alexey.nesterov@live.com>
2017-05-23 21:21:16 +0100

    Add sample code

---
+++ src/test/kotlin/com/example/SpringCoroutinesApplicationTests.kt
@@ -0,0 +1,11 @@
+package com.example
+
+import org.junit.runner.RunWith
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+@AutoConfigureWebTestClient
+class SpringCoroutinesApplicationTests
