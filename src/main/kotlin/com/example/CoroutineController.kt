29481a8ac17ed373a4d91642a49350c85b232479
Alexey Nesterov <alexey.nesterov@live.com>
2017-05-30 21:40:13 +0100

    Fix inconsistency in usage extension methods

--- src/main/kotlin/com/example/CoroutineController.kt
+++ src/main/kotlin/com/example/CoroutineController.kt
@@ -4,7 +4,6 @@ import com.example.persistence.ReactiveAuditRepository
 import com.example.persistence.ReactiveMessageRepository
 import com.example.persistence.ReactivePeopleRepository
 import kotlinx.coroutines.experimental.Unconfined
-import kotlinx.coroutines.experimental.reactive.awaitFirst
 import kotlinx.coroutines.experimental.reactive.awaitSingle
 import kotlinx.coroutines.experimental.reactor.mono
 import org.springframework.web.bind.annotation.GetMapping
@@ -20,8 +19,8 @@ class CoroutineController(
     @GetMapping("/coroutine/{personId}")
     fun getMessages(@PathVariable personId: String) = mono(Unconfined) {
         val person = peopleRepository.findById(personId).awaitSingle()
-        val lastLogin = auditRepository.findByEmail(person.email).awaitFirst().eventDate
-        val numberOfMessages = messageRepository.countByMessageDateGreaterThanAndEmail(lastLogin, person.email).awaitFirst()
+        val lastLogin = auditRepository.findByEmail(person.email).awaitSingle().eventDate
+        val numberOfMessages = messageRepository.countByMessageDateGreaterThanAndEmail(lastLogin, person.email).awaitSingle()
 
         val message = "Hello ${person.name}, you have $numberOfMessages messages since $lastLogin"
 
